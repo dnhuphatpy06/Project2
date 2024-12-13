e0f6eb3bbc04e77abfa12aeea3e12c16036370bf
GiaHuy-2546 <12345huy123@gmail.com>
2024-12-13 22:50:31 +0700

    vers 2.0

--- App/app.py
+++ App/app.py
@@ -23,8 +23,6 @@ import logging
 logging.getLogger('absl').setLevel(logging.ERROR)
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
-# check_don = IntVar()
-
 print("""Chào mừng đến với ỨNG DỤNG NHẬN DIỆN CẢM XÚC TỪ GIỌNG NÓI!
 
 Ban đầu có thể mô hình sẽ load hơi lâu, hãy đợi một chút!      
@@ -34,16 +32,23 @@ Hướng dẫn sơ bộ:
 2. Nút 'Ghi âm ngắn': Ghi âm âm thanh trực tiếp trong 2 giây và sau đó đưa ra dự đoán.
 3. Nút 'Ghi âm dài': Ghi âm âm thanh trực tiếp cho đến khi bấm dừng.
 4. Nút 'Upload file .wav': Tải file âm thanh .wav để mô hình đưa ra dự đoán.
-5. Ô 'Chọn mô hình': Chọn mô hình mà bạn muốn dùng để dự đoán. Lưu ý: Hãy chọn đúng tên mô hình!
+5. Nút 'Dự đoán 1 lần': Điều chỉnh việc file âm thanh được dự đoán 1 lần hay được chia ra thành các đoạn để dự đoán.
+6. Ô 'Chọn mô hình': Chọn mô hình mà bạn muốn dùng để dự đoán. Lưu ý: Hãy chọn đúng tên mô hình!
 
 Chúc bạn có trải nghiệm tốt nhất!""")
 
 cam_xuc = ('Giận dữ', 'Sợ hãi', 'Hạnh phúc', 'Bình thường', 'Buồn bã')
 tuong_ung = ('ANG','FEA', 'HAP', 'NEU', 'SAD')
 
+def max_appear(list_emo):
+    dic = {}
+    for i in list_emo:
+        dic[i] = dic.get(i,0) + 1
+    return max(dic.items(), key = lambda a: a[1])[0]
+    
 def chay_thanh():
     progress_bar['value'] = 0
-    progress_bar.grid(row=6, column=0, pady=20)
+    progress_bar.grid(row=7, column=0, pady=20)
 
 def extract_feature(X, sr):
     X, sample_rate = X, sr
@@ -102,10 +107,12 @@ def record_lien_tuc():
     try:
         global recorded_data
         recorded_data = []
-        hien_thi_2.config(text='Đang ghi âm...')
-        option.update()
+        
         stream = sounddevice.InputStream(callback=callback, channels=2, samplerate=44100)
         stream.start()
+        nut_dung.grid(row=0, column=1, padx=10)
+        hien_thi_2.config(text='Đang ghi âm...')
+        option.update()
         while not stop_event.is_set():
             pass
         stream.stop()
@@ -118,42 +125,55 @@ def record_lien_tuc():
         list_emo = predict_dai('./audioL.wav', modelpath[0])
         hien_thi_2.config(text='Hoàn tất!')
         option.update()
-        y, sr = librosa.load('./audioL.wav')
-        sounddevice.play(y, sr)
-        chay_thanh()
-        time.sleep(0.5)
-        step = 100 / len(list_emo)
-        for i in range(len(list_emo)):
-            hien_du_doan(list_emo[i])
-            progress_bar['value'] += step
+        if not check_don.get():
+            y, sr = librosa.load('./audioL.wav')
+            sounddevice.play(y, sr)
+            chay_thanh()
+            step = 100 / (len(list_emo) - 1) ######################################
+            for i in range(len(list_emo)):
+                hien_du_doan(list_emo[i])
+                time.sleep(0.5)
+                if progress_bar['value'] < 100:
+                    progress_bar['value'] += step
             time.sleep(0.5)
-        hien_thi_2.config(text='')
-        option.update()
+            progress_bar.grid_forget()
+            hien_chu_hang_7.config(text='Kết quả tổng quát cuối cùng:',font=('cambria', 15))
+            hien_chu_hang_7.grid(row=0, column=0)
+            hang_7.grid(row=7, column=0,pady=10)
+            hien_du_doan(max_appear(list_emo))
+        else:
+            hien_du_doan(max_appear(list_emo))
+        
+        
     except sounddevice.PortAudioError:
         hien_thi_2.config(text='Không thể ghi âm!')
         option.update()
     
-def record_it_giay():
-    try:
-        hien_thi_1.config(text='Bắt đầu')
-        option.update()
-        fs = 44100
-        seconds= 2
-        myrecording = sounddevice.rec(int(seconds*fs), samplerate = fs, channels = 2)
-        sounddevice.wait()
-        write('./audio.wav', fs, myrecording)
-        hien_thi_1.config(text='Kết thúc')
-        option.update()
-        y, sr = librosa.load('./audio.wav', sr=None)
-        return predict(y, sr, modelpath[0])
-    except sounddevice.PortAudioError:
-        hien_thi_1.config(text='Không thể ghi âm!')
-        option.update()
-
+# def record_it_giay():
+#     try:
+#         hien_thi_1.config(text='Bắt đầu')
+#         option.update()
+#         fs = 44100
+#         seconds= 2
+#         myrecording = sounddevice.rec(int(seconds*fs), samplerate = fs, channels = 2)
+#         sounddevice.wait()
+#         write('./audio.wav', fs, myrecording)
+#         hien_thi_1.config(text='Kết thúc')
+#         option.update()
+#         y, sr = librosa.load('./audio.wav', sr=None)
+#         return predict(y, sr, modelpath[0])
+#     except sounddevice.PortAudioError:
+#         hien_thi_1.config(text='Không thể ghi âm!')
+#         option.update()
+""""""
 root = Tk()
 
 root.title('NHẬN DIỆN CẢM XÚC TỪ GIỌNG NÓI')
-root.minsize(height=500, width=500)
+# root.minsize(height=500, width=500)
+root.geometry("925x500")
+root.resizable(False, False)
+###Bien:
+check_don = IntVar()
 
 #Hang 0:
 Button(root, text='Thoát', command=root.quit).grid(row=0, column=0,sticky='w')
@@ -163,12 +183,12 @@ Label(root, text='Chọn chế độ:',font=('cambria', 24), width=50).grid(row=
 stop_event = threading.Event()
 
 option = Frame(root)
-def record_ngan():
-    xoa_hien_thi()
-    try:
-        hien_du_doan(record_it_giay())
-    except:
-        pass
+# def record_ngan():
+#     xoa_hien_thi()
+#     try:
+#         hien_du_doan(record_it_giay())
+#     except:
+#         pass
     
 def record_dai():
     xoa_hien_thi()
@@ -176,12 +196,12 @@ def record_dai():
     stop_event.clear()
     try:
         threading.Thread(target=record_lien_tuc, daemon=True).start()
-        # time.sleep(4)
-        nut_dung.grid(row=0, column=1, padx=10)
+        # nut_dung.grid(row=0, column=1, padx=10)
     except:
         pass
 
 def xoa_hien_thi():
+    hang_7.grid_forget()
     duong_dan.config(text="")
     hien_thi_1.config(text="")
     hien_thi_2.config(text="")
@@ -209,8 +229,8 @@ def stop_recording():
     stop_event.set()
     nut_dung.grid_forget()
 
-Button(option, text="Ghi âm ngắn", width=30, command=record_ngan).grid(row=0, column=0, padx=10)
-Button(option, text='Ghi âm dài', width=30, command=record_dai).grid(row=0, column=1, padx=10)
+# Button(option, text="Ghi âm ngắn", width=30, command=record_ngan).grid(row=0, column=0, padx=10)
+Button(option, text='Ghi âm', width=30, command=record_dai).grid(row=0, column=1, padx=10)
 nut_dung = Button(option, text='Dừng', width=30, command=stop_recording)
 def upload_file():
     xoa_hien_thi()
@@ -219,27 +239,30 @@ def upload_file():
     if file_path:
         duong_dan.config(text=f"File đã chọn: {file_path.split('/')[-1]}")
         option.update()
-        list_emo = predict_dai(file_path, modelpath[0])
         y, sr = librosa.load(file_path)
-        sounddevice.play(y, sr)
-        chay_thanh()
-        step = 100 / len(list_emo)
-        time.sleep(0.5)
-        for i in range(len(list_emo)):
-            hien_du_doan(list_emo[i])
-            progress_bar['value'] += step
+        list_emo = predict_dai(file_path, modelpath[0])
+        if not check_don.get():
+            sounddevice.play(y, sr)
+            chay_thanh()
+            step = 100 / (len(list_emo) - 1) ########################################
+            # time.sleep(0.5)
+            for i in range(len(list_emo)):
+                hien_du_doan(list_emo[i])
+                time.sleep(0.5)
+                progress_bar['value'] += step
             time.sleep(0.5)
+            progress_bar.grid_forget()
+            hien_chu_hang_7.config(text='Kết quả tổng quát cuối cùng:',font=('cambria', 15))
+            hien_chu_hang_7.grid(row=0, column=0)
+            hang_7.grid(row=7, column=0, pady=10)
+            hien_du_doan(max_appear(list_emo))
+        else:
+            hien_du_doan(max_appear(list_emo))
+            
         
 Button(option, text='Upload file .wav', width=30, command=upload_file).grid(row=0, column=2)
 option.grid(row=2, column=0, pady=10)
-# bien_check = [0]
-# def check_1_lan():
-#     if check_don.get():
-#         bien_check[0] = 1
-#     else:
-#         bien_check[0] = 0
-# du_doan_don = Checkbutton(option, text='Dự đoán 1 lần', variable=check_don, command=check_1_lan)
-#
+
 hien_thi_1 = Label(option, text="",font=('cambria', 10))
 hien_thi_1.grid(row=1, column=0,pady=5)
 hien_thi_2 = Label(option, text="",font=('cambria', 10))
@@ -272,17 +295,23 @@ def lay_mo_hinh():
 Button(chon_mo_hinh, text="Chọn", command=lay_mo_hinh).grid(row=0,column=2)
 chon_mo_hinh.grid(row=4, column=0, pady=10)
 ### 5
-Label(root, text='Dự đoán:',fg='red', font=('cambria', 16), width=40).grid(row=5,column=0,pady=10)
+du_doan_don = Checkbutton(root, text='Chỉ hiện kết quả cuối', variable=check_don)
+du_doan_don.grid(row=5, column=0, pady=5)
 ### 6
+Label(root, text='Dự đoán:',fg='red', font=('cambria', 16), width=40).grid(row=6,column=0,pady=10)
+### 7
 progress_bar = Progressbar(root, orient="horizontal", length=300, mode="indeterminate")
 progress_bar.grid_forget()
+hang_7 = Frame(root)
+hien_chu_hang_7 = Label(hang_7, text='')
+hang_7.grid(row=7, column=0, pady=5)
 ### 7
 dudoan = Frame(root)
 anh = Label(dudoan)
 anh.grid(row=1, column=0, padx=50)
 du_doan = Label(dudoan, text='',font=('cambria', 15))
 du_doan.grid(row=1, column=1)
-dudoan.grid(row=7, column=0)
+dudoan.grid(row=8, column=0)
 ###
 
 root.mainloop()
